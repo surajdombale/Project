5f7bee37819e25099f38a2ac59c9fbd8c43ae9c0
Tejas <tejas24497@gmail.com>
2023-09-05 17:08:27 +0530

    Main class

--- shopme/shopmeWebParent/shopmeBackEnd/src/main/java/com/shopme/controller/SpringBootApplication.java
+++ shopme/shopmeWebParent/shopmeBackEnd/src/main/java/com/shopme/controller/SpringBootApplication.java
@@ -2,9 +2,13 @@ package com.shopme.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 @EntityScan(basePackages = { "com.shopme" })
+@ComponentScan(basePackages = { "com.shopme" })
+@EnableJpaRepositories("com.shopme.repo")
 public class SpringBootApplication {
 
 	public static void main(String[] args) {
